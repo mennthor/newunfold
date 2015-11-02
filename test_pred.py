e78f18bb8d1053c8416713be348d42361bc8b46d
Thorben Menne <tmenne@MacMini03-142a.local>
2015-11-02 08:29:11 +0100

    comnpare without regulariztion

---
+++ test_pred.py
@@ -0,0 +1,57 @@
+#-*-coding:utf8-*-
+
+import numpy as np
+import matplotlib.pyplot as plt
+import mc_data_gen
+import unfold
+
+
+N = 100000
+xl = 0
+xh = 2
+
+mcd = mc_data_gen.FlatUnfoldData(N=N, range=[xl, xh])
+true, meas = mcd.get_mc_sample()
+
+unf = unfold.unfold(
+	range_true=[xl, xh],
+	range_meas=[xl, xh],
+	nbins_true=20,
+	nbins_meas=20,
+	)
+
+A = unf.fit(true["data"], meas["data"], meas["weight"])
+
+# Unfold the true distribtuion used for training to test the unfolding
+predicted_res = unf.predict(true["data"])
+
+# Simpe inversion for comparison
+predicted_simple_inv = unf.predict_by_inverse(true["data"])
+
+# Bin true MC for comparison
+hist, bins = np.histogram(true["data"], bins=20, range=[xl, xh], density=False)
+# print("True :")
+# print(hist)
+
+# print("")
+# print("Predicted :")
+# print(predicted_res.x)
+
+# print("")
+# print("Simple inv pred :")
+# print(predicted_simple_inv)
+
+# Plot
+fig, ax = plt.subplots(1, 1)
+
+binmids = 0.5 * (bins[:-1] + bins[1:])
+ax.plot(binmids, hist, drawstyle="steps-mid")
+ax.plot(binmids, predicted_res.x, drawstyle="steps-mid")
+ax.plot(binmids, predicted_simple_inv, drawstyle="steps-mid")
+
+ax.set_yscale("log", nonposy="clip")
+ax.set_ylim((1, 1e5))
+
+ax.set_xlabel("bin number")
+ax.set_ylabel("entries")
+plt.show()
--- unfold.py
+++ unfold.py
@@ -1,6 +1,7 @@
 #-*-coding:utf8-*-
 
 import numpy as np
+import scipy.optimize as sco
 
 class unfold():
 	"""
@@ -16,6 +17,9 @@ class unfold():
 	The method fit trains the model on training data, eg. MC data.
 
 	The method predict applies the trained model to test or real data.
+	The number of bins used to bin the true MC distribution is the same as the
+	number of bins used for the binning of the unfolded data. This is because
+	the ranks of the matrix and the vector must match in the unfolding equation.
 	"""
 	def __init__(self, **kwargs):
 		"""
@@ -83,7 +87,7 @@ class unfold():
 		# of the true distribution x, which are measured in bin i of the
 		# measured distributioin y.
 		# So A has nbins_meas rows (index i) and nbins_true cols (index j)
-		A = np.zeros((self.nbins_meas, self.nbins_true))
+		self.A = np.zeros((self.nbins_meas, self.nbins_true))
 		# Loop over bin indices and count events
 		for j in np.arange(0, self.nbins_true):
 			for i in np.arange(0, self.nbins_meas):
@@ -93,23 +97,136 @@ class unfold():
 					bin_index_true == j+1,
 					bin_index_meas == i+1,
 					)
+				# Nor every column by bin entry true_j. TODO: Use true weights
+				# Without acceptance it would be: sum_i aij/norm_j = 1
+				norm_j = np.sum(bin_index_true == j+1)
 				# Sum over measured event weights to take the acceptance
 				# into account
-				A[i][j] = np.sum(meas_weights[mask])
+				self.A[i][j] = np.sum(meas_weights[mask]) / norm_j
 
-		return A
+		return self.A
 
 
-	def predict(self):
+	def predict(self, data, data_weights=None):
 		"""
-		Use trained model on prediction data.
+		Use trained model matrix A on prediction data.
+
+		Returns
+		-------
+		pred : ndarray
+			Array of predicted, unfolded distribution, calculated by applying
+			the trained response matrix to the real measured data.
 		"""
+		def chi2(x, *args):
+			"""
+			Approximating chi-square fit to obtain initial values for the
+			likelihood fit.
+
+			Returns
+			-------
+			chi2 : float
+				Value of the minimizer function for a specific choice of x
+			"""
+			y_meas = np.array(args)
+			# Expected number of events for a specific choice of x
+			y = np.dot(self.A, x)
+			chi2 = np.sum( (y_meas - y)**2 / y_meas )
+
+			return chi2
+
+		def llh(x, *args):
+			"""
+			Negative log-likelihood function used to fit the components
+			of vector x, describing the bin contents of the unfolded
+			distribution.
+
+			Parameters
+			----------
+			x : ndarray
+				Function values which ought to be minimized.
+			*args : tuple
+				Additional paramters:
+					arg[0] : ndarray of histogrammed measured data
+
+			Returns
+			-------
+			llh : float
+				Value of the minimizer function for a specific choice of x
+			"""
+			y_meas = np.array(args)
+			# Expected number of events for a specific choice of x
+			y = np.dot(self.A, x)
+			llh = np.sum( y - y_meas * np.log(y))
+
+			return llh
+
+		# First, bin the measured data. No need to track anything, just
+		# make a simple histogram with nbins_meas as in the fit method.
+		# No need for the bin edges, so discard.
+		meas, _ = np.histogram(
+			data,
+			bins=self.nbins_meas,
+			range=[self.range_meas[0], self.range_meas[1]],
+			density=False,
+			weights=data_weights,
+			)
+
+		# Minimizer setup
+		# The measured binned data is needed in the fit functions
+		args = tuple(meas)
+		method = "L-BFGS-B"
+		# Hist entries need to be positive
+		bounds = tuple((0, None) for i in range(self.nbins_true))
+
+		# Init preliminary fir with the measured bin entries
+		x0 = meas
+		x0 = np.ones_like(meas)
+		# get preliminary fit to use as initial values for the likelihood fit
+		preliminary_res = sco.minimize(
+			chi2,
+			x0,
+			args=args,
+			bounds=bounds,
+			method=method,
+			jac=False
+			)
+
+		# Now do the llh fit with the previous fit result as initial values
+		x0 = preliminary_res.x
+		print("Preliminary :  {}".format(x0))
+		predicted_res = sco.minimize(
+			llh,
+			x0,
+			args=args,
+			bounds=bounds,
+			method=method,
+			jac=False
+			)
+
+		return predicted_res
 
 
+	def predict_by_inverse(self, data, data_weights=None):
+		"""
+		Unfold by simple matrix inversion
+		"""
+		s = np.shape(self.A)
+		if(not len(s) == 2):
+			if(not s[0] == s[1]):
+				print("Error: Simple inversion only works with a square matrix.")
+				return
 
-		return
+		meas, _ = np.histogram(
+		data,
+		bins=self.nbins_meas,
+		range=[self.range_meas[0], self.range_meas[1]],
+		density=False,
+		weights=data_weights,
+		)
 
+		predicted = np.dot(np.linalg.inv(self.A), meas)
 
+		return predicted
 
 
 
