added regularization. TODO: Normalization of x gets wrong, because of added reg term. Reg doesn't work as expected. Use reg also in preliminary fit?

mc_data_gen.py
@@ -27,6 +27,9 @@ class NullUnfoldData(object):
 		If range[0] is <0, the shift function shifts every number<0 to 0.
 	N : int
 		Number of points sampled from the pdf for the true distribution
+	seed : int
+		Seed for the np.random.RandomState random number generator.
+		Default=11235813
 
 	Functions
 	---------
@@ -45,6 +48,11 @@ class NullUnfoldData(object):
 		self.N = kwargs.pop("N", 10000)
 		self.xl = self.range[0]
 		self.xh = self.range[1]
+		self.seed = kwargs.pop("seed", 11235813)
+
+		# Create the random number generator with fixed seed to be able to
+		# reproduce results
+		self.rs = np.random.RandomState(self.seed)
 
 
 	def get_mc_sample(self):
@@ -109,7 +117,7 @@ class NullUnfoldData(object):
 		yl = 0.5
 		ym = 1
 		yh = 0.5
-		acceptance = ( np.random.uniform(size=self.N) <=
+		acceptance = ( self.rs.uniform(size=self.N) <=
 			self._parabola(data, yl, ym, yh) )
 		# If not accepted, set weight to 0
 		weights[~acceptance] = 0.
@@ -132,7 +140,7 @@ class NullUnfoldData(object):
 		presmeared = np.copy(measured)
 		# If values are smeareded to outside the bounds try again
 		while np.sum(overflow)>0:
-			measured[overflow] = presmeared[overflow] + np.random.normal(
+			measured[overflow] = presmeared[overflow] + self.rs.normal(
 				loc=0, scale=sigma, size=np.sum(overflow))
 			overflow = np.logical_or(measured<self.xl, measured>self.xh)
 
@@ -264,11 +272,11 @@ class LorentzianUnfoldData(NullUnfoldData):
 		# is above N, use the first N random numbers for the sample.
 		while len(data)<=self.N:
 			# Comparison function g(vn)=k
-			vn = np.random.uniform(low=self.xl, high=self.xh, size=self.N)
+			vn = self.rs.uniform(low=self.xl, high=self.xh, size=self.N)
 			# Get pdf values from f(vn), which shall be sampled from
 			fvn = self._pdf(vn, self.xl, self.xh)
 			# Accept if vn * uniform[0,1] * g(vn) < f(vn)
-			accept = (k  * np.random.uniform(size=self.N) < fvn)
+			accept = (k  * self.rs.uniform(size=self.N) < fvn)
 			# Append all accepted
 			data.extend(vn[accept])
 			# Count total generated randonm numbers for performance information
@@ -312,7 +320,7 @@ class FlatUnfoldData(NullUnfoldData):
 			(ID, data, weight)
 		"""
 		# Generate N unfiormly distributed events in [xl, xh]
-		self.data = np.random.uniform(low=self.xl, high=self.xh, size=self.N)
+		self.data = self.rs.uniform(low=self.xl, high=self.xh, size=self.N)
 
 		# Combine to recarray with fields (ID, data, weight)
 		self.true = np.empty((self.N, ), dtype=[

unfold.py
@@ -17,9 +17,12 @@ class unfold():
 	The method fit trains the model on training data, eg. MC data.
 
 	The method predict applies the trained model to test or real data.
+
+	Note:
 	The number of bins used to bin the true MC distribution is the same as the
 	number of bins used for the binning of the unfolded data. This is because
-	the ranks of the matrix and the vector must match in the unfolding equation.
+	the number of rows of the matrix and the vector must match in the
+	unfolding equation.
 	"""
 	def __init__(self, **kwargs):
 		"""
@@ -29,8 +32,6 @@ class unfold():
 
 		Parameters
 		----------
-		ndof : float
-			Degress of freedom for the regularization. Default: 10.
 		nbins_* : int
 			*true : Number of bins for the true MC trainig data.
 			*meas : Number of bins for the measured MC training data.
@@ -42,7 +43,7 @@ class unfold():
 			*pred : Range for the binning of the predicted unfolded data.
 			Default ranges are [0, 1].
 		"""
-		self.ndof = kwargs.pop("ndof", 10)
+		# self.ndof = kwargs.pop("ndof", 10)
 
 		self.range_true = kwargs.pop("range_true", [0, 1])
 		self.range_meas = kwargs.pop("range_meas", [0, 1])
@@ -74,8 +75,10 @@ class unfold():
 		# Create equally spaced bins in given range
 		bins_true = np.linspace(
 			self.range_true[0], self.range_true[1], self.nbins_true + 1)
+		self.h_true = bins_true[1] - bins_true[0]
 		bins_meas = np.linspace(
 			self.range_meas[0], self.range_meas[1], self.nbins_meas + 1)
+		self.h_meas = bins_meas[1] - bins_meas[0]
 
 		# Get bin indices for each single event
 		bin_index_true = np.digitize(true, bins=bins_true)
@@ -107,9 +110,14 @@ class unfold():
 		return self.A
 
 
-	def predict(self, data, data_weights=None):
+	def predict(self, data, data_weights=None, ndof=10):
 		"""
 		Use trained model matrix A on prediction data.
+
+		Parameters
+		----------
+		ndof : float
+			Degress of freedom for the regularization. Default: 10.
 
 		Returns
 		-------
@@ -117,6 +125,29 @@ class unfold():
 			Array of predicted, unfolded distribution, calculated by applying
 			the trained response matrix to the real measured data.
 		"""
+		def reg(x):
+			"""
+			Return the regularization term for a given set of bin entires x
+			describing the target function.
+
+			Calculate regularization term dependent on the curvature of
+			histogram described by the current x values, which is the
+			discretized integral over second derivation squared:
+				   int (f''(x))^2 dx
+				-> sum (f''(x))^2 * h
+			with the second numerical derivate at every position x[j]
+				f'' = (f(x[j-1]) - 2f(x[j]) + f(x[j+1])) / h^2
+			and the bin width (here equal for every bin)
+				h = x[j] - x[j-1] = x[1] - x[0] = const
+			"""
+			f2_squared = 0.
+			for j in np.arange(1, self.nbins_true-1):
+				f2_squared += (x[j-1] - 2*x[j] + x[j+1]) / self.h_true**3
+
+			regularization = 0.5 * ndof * f2_squared
+
+			return regularization
+
 		def chi2(x, *args):
 			"""
 			Approximating chi-square fit to obtain initial values for the
@@ -130,7 +161,7 @@ class unfold():
 			y_meas = np.array(args)
 			# Expected number of events for a specific choice of x
 			y = np.dot(self.A, x)
-			chi2 = np.sum( (y_meas - y)**2 / y_meas )
+			chi2 = np.sum( (y_meas - y)**2 / y_meas ) + reg(x)
 
 			return chi2
 
@@ -156,7 +187,7 @@ class unfold():
 			y_meas = np.array(args)
 			# Expected number of events for a specific choice of x
 			y = np.dot(self.A, x)
-			llh = np.sum( y - y_meas * np.log(y))
+			llh = np.sum( y - y_meas * np.log(y)) + reg(x)
 
 			return llh
 